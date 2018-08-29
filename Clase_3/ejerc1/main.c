9d6a8d86e7d01d7a26ad6e2781972f0f1ff3d836
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-08-28 21:55:47 -0300

    Add files via upload

--- Clase_3/ejerc1/main.c
+++ Clase_3/ejerc1/main.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MIN 1
+#define MAX 100
+
+int pedirEdad();
+
+int validarEdad(int* edad,int edadMax,int edadMin);
 
 int main()//el asterisco(*) se llama operador de indireccion
 {
-    printf("Hello world!\n");
+    int edadValida = 0;
+    int edad = 0;
+    int repetir = 1;
+    do
+    {
+        edadValida = validarEdad(&edad, MAX, MIN);
+        if(edadValida)
+        {
+            printf("La edad del usuario es %d\n",edad);
+        }
+        else
+        {
+            printf("Error: la edad del usuario no es valida\n");
+        }
+        printf("Desea reingresar la edad del usuario? <1/0>\n");
+        scanf("%d",&repetir);
+    }while(repetir == 1);
     return 0;
 }
+
+int pedirEdad()
+{
+    int edad;
+    printf("Ingrese la edad del usuario: ");
+    scanf("%d",&edad);
+    return edad;
+}
+
+int validarEdad(int* edad,int edadMax,int edadMin)
+{
+    int respuesta = 0, iteracion;
+    int* pEdad = edad;
+    *pEdad = pedirEdad();
+    for(iteracion = 0;iteracion < 3;iteracion++)
+    {
+        if(*pEdad >= edadMin && *pEdad <= edadMax)
+        {
+            respuesta = 1;
+        }
+    }
+    return respuesta;
+}
+
+//printf("La edad del usuario es %d\n",edad);
+//printf("Error: la edad del usuario no es valida\n");
