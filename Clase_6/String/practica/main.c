61bf9c18751f57f1483f8364846d48a1dcd3095f
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-09-10 21:44:19 -0300

    Add files via upload

--- Clase_6/String/practica/main.c
+++ Clase_6/String/practica/main.c
@@ -14,5 +14,23 @@ int main()//strcpy(destino,origen);
     char nombre[31];
     char apellido[31];
     char apeNom[62];
+    printf("Nombre: ");
+    fflush(stdin);
+    fgets(nombre,sizeof(nombre)-2,stdin);
+    printf("Apellido: ");
+    fflush(stdin);
+    fgets(apellido,sizeof(apellido)-2,stdin);
+    //tengo 2 maneras
+    //1-
+    /*strcpy(apeNom,apellido);
+    strcat(apeNom," ");
+    strcat(apeNom,nombre);*/
+    //2-
+    apeNom[0]='\0';
+    strcat(apeNom,apellido);
+    strcat(apeNom," ");
+    strcat(apeNom,nombre);
+
+    printf("%s",apeNom);
     return 0;
 }
