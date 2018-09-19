d2648ea2c5193cfc3f87807ed2c6ad55b86a16dc
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-09-18 22:15:39 -0300

    Add files via upload

--- Clase_8/Ejercicio/ABM.h
+++ Clase_8/Ejercicio/ABM.h
@@ -15,14 +15,48 @@ char apellido[31];
 fecha fechaNacimiento;
 }persona;
 
+/** \brief Inicializa el array de empleados poniendolos como vacios
+ *
+ * \param eEmpleado[] persona   Array de empleados
+ * \param cantidad int  Cantidad de empleados del array
+ * \return void
+ *
+ */
 void abm_Inicializar(persona eEmpleado[],int cantidad);
 
+/** \brief Imprime el menu de opciones principales
+ *
+ * \return void
+ *
+ */
 void abm_Menu();
 
+/** \brief Da de alta empleados hasta que ocurra un error
+ *
+ * \param eEmpleado[] persona   Array de empleados
+ * \param cantidad int  Cantidad de empleados del array
+ * \return void
+ *
+ */
 void abm_Alta(persona eEmpleado[],int cantidad);
 
+/** \brief Toma los datos de un empleado para darlo de alta
+ *
+ * \param eEmpleado[] persona   Array de empleados
+ * \param vacio int Indice del primer espacio vacio del array
+ * \param cantidad int  Cantidad de empleados del array
+ * \return int  Devuelve -1 en caso de error y 0 si se logra completar
+ *
+ */
 int abm_ingresaEmpleado(persona eEmpleado[],int vacio,int cantidad);
 
+/** \brief Busca el primer espacio vacio en el array
+ *
+ * \param eEmpleado[] persona   Array de empleados
+ * \param cantidad int  Cantidad de empleados del array
+ * \return int Devuelve el indice del espacio vacio
+ *
+ */
 int abm_espacioVacio(persona eEmpleado[],int cantidad);
 
 void abm_imprimirEmpleados(persona eEmpleado[],int cantidad);
