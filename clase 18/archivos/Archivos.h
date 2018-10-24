14b1117d570ba715fec481a008595996027c013b
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-10-23 21:26:26 -0300

    Add files via upload

---
+++ clase 18/archivos/Archivos.h
@@ -0,0 +1,12 @@
+#ifndef ARCHIVOS_DINAMICA_H_INCLUDED
+#define ARCHIVOS_DINAMICA_H_INCLUDED
+
+
+
+
+
+
+
+
+
+#endif
---
+++ clase 18/archivos/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Archivos.c"
+#define ARCHIVO "data.csv"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
